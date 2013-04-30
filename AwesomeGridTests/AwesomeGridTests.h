8e516a9e623792f79e9f4c29fec1dae131ad4315
Sam Sharaf <m372520@A3836944.local>
2013-04-29 22:11:35 -0700

    Initial Commit

---
+++ AwesomeGridTests/AwesomeGridTests.h
@@ -0,0 +1,13 @@
+//
+//  AwesomeGridTests.h
+//  AwesomeGridTests
+//
+//  Created by Sam Sharaf on 4/29/13.
+//  Copyright (c) 2013 Sam Sharaf. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AwesomeGridTests : SenTestCase
+
+@end
---
+++ AwesomeGridTests/AwesomeGridTests.m
@@ -0,0 +1,32 @@
+//
+//  AwesomeGridTests.m
+//  AwesomeGridTests
+//
+//  Created by Sam Sharaf on 4/29/13.
+//  Copyright (c) 2013 Sam Sharaf. All rights reserved.
+//
+
+#import "AwesomeGridTests.h"
+
+@implementation AwesomeGridTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AwesomeGridTests");
+}
+
+@end
